c00aca1b989297cad325db187b176ef409d28068
Antoine Marechal <antoinemarechal08@gmail.com>
2024-12-13 14:50:17 +0100

    rename util.py to utils.py and remove debug print statement

--- app/utils.py
+++ app/utils.py
@@ -4,5 +4,4 @@ import bcrypt
 def hash_password(password):
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
-    print(hashed)
     return hashed.decode("utf-8")
