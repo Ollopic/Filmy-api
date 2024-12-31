5ee4cb351215a3d6b66373b55ce3b4c18808b88b
Antoine Marechal <antoinemarechal08@gmail.com>
2024-12-31 15:07:46 +0100

    test: :white_check_mark: add tests for user login

---
+++ tests/test_auth.py
@@ -0,0 +1,33 @@
+def test_login_user_success(client):
+  """Test de connexion d'un utilisateur"""
+  response = client.get(
+      "/user/login",
+      json={"mail": "admin@example.com", "password": "admin"},
+  )
+
+  assert response.status_code == 200
+  assert "token" in response.json
+  assert response.json["message"] == "User logged in successfully"
+
+
+def test_login_user_invalid_password(client):
+  """Test de connexion avec un mauvais mot de passe"""
+  response = client.get(
+      "/user/login",
+      json={"mail": "admin@example.com", "password": "wrongpassword"},
+  )
+
+  assert response.status_code == 401
+  assert response.json == {"error": "Invalid password"}
+
+
+def test_login_user_not_found(client):
+  """Test de connexion avec un utilisateur inexistant"""
+  response = client.get(
+      "/user/login",
+      json={"mail": "nonexistent@example.com", "password": "password"},
+  )
+
+  assert response.status_code == 404
+  assert response.json == {"error": "User not found"}
+
