202e743bfc735ae4e7b45b7b54f28edb8e732da5
Antoine Marechal <antoinemarechal08@gmail.com>
2025-01-11 12:49:04 +0100

    feat: add popular and search routes for person

--- app/routes/person_routes.py
+++ app/routes/person_routes.py
@@ -1,9 +1,29 @@
+from flask import request
+
 from app.app import app
 from app.db.database import db
 from app.db.models import Person
 from app.themoviedb.client import Client
 
 tmdb_client = Client()
+
+
+@app.route("/person", methods=["GET"])
+def search_person():
+    name = request.args.get("name")
+    data = tmdb_client.get_person_by_name(name)["results"]
+
+    result = [
+        {
+            "id_tmdb": person["id"],
+            "name": person["name"],
+            "profile_path": person["profile_path"],
+            "known_for": person["known_for_department"],
+        }
+        for person in data
+    ]
+
+    return result, 200
 
 
 @app.route("/person/<int:identifier>", methods=["GET"])
@@ -24,3 +44,20 @@ def get_person(identifier: int):
     return {
         "data": person.data,
     }, 200
+
+
+@app.route("/person/popular", methods=["GET"])
+def get_popular_person():
+    data = tmdb_client.get_popular_person()["results"]
+
+    result = [
+        {
+            "id_tmdb": person["id"],
+            "name": person["name"],
+            "profile_path": person["profile_path"],
+            "known_for": person["known_for_department"],
+        }
+        for person in data
+    ]
+
+    return result, 200
