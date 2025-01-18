d73a99e33c6ee28b27ee8e74d01fcf08a2b6612c
gaétan-lemont <gaetanl2004@gmail.com>
2025-01-18 18:17:50 +0100

    Add pagination to endpoints

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 from app.config import (
     jwtAccessTokenExpiresConfig,
@@ -16,3 +17,5 @@ from app.routes import *  # noqa: E402, F403
 app.config["JWT_SECRET_KEY"] = jwtSecretKeyConfig
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=jwtAccessTokenExpiresConfig)
 jwt = JWTManager(app)
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
